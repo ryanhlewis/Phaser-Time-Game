bae0eded4a55db959c5c54b8d26081ee29cf6ceb
AlyssaBurtscher <98368969+AlyssaBurtscher@users.noreply.github.com>
2022-02-19 21:01:12 -0600

    Animated Player
    Moves left and right and is animated, but does not jump and moves left backwards.

--- game.js
+++ game.js
@@ -2,27 +2,9 @@
 // Made for CS329E: Elements of Game Development
 // By Alyssa, Angela, and Ryan
 
-var config = {
-    type: Phaser.AUTO,
-    width: 640,
-    height: 640,
-    pixelArt: true,
-    physics: {
-        default: "arcade",
-        arcade: {
-            gravity: { y: 300 },
-        },
-    },
-    scene: [
-        LoadAssets/*,
-        InGame*/        
-    ],
-};
-
-var game = new Phaser.Game(config);
-
 // Global Variables
 var player;
+var cursors;
 
 class LoadAssets extends Phaser.Scene {
     // Load all assets for the first time, never again.
@@ -30,13 +12,51 @@ class LoadAssets extends Phaser.Scene {
         super("LoadAssets");
     }
     preload() {
-        this.load.image('lab', 'assets/lab.png')
-        this.load.spritesheet('player', 'assets/player.png', { frameWidth: 32, frameHeight: 32 })
+        this.load.spritesheet('player', 'assets/player.png', { frameWidth: 48, frameHeight: 48 })
     }
     create() {
-        this.add.image(0, 0, 'lab').setOrigin(0, 0);
         player = this.physics.add.sprite(15, 250, 'player');
+        this.anims.create({
+            key: 'stand',
+            frames: [ { key: 'player', frame: 0 } ],
+            frameRate: 10
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('player', { start: 6, end: 11 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('player', { start: 6, end: 11 }),
+            frameRate: 10,
+            repeat: -1
+        });
+        cursors = this.input.keyboard.createCursorKeys();
     }
+    update() {
+            if (cursors.left.isDown)
+            {
+                player.setVelocityX(-50);
+
+                player.anims.play('left', true);
+            }
+            else if (cursors.right.isDown)
+            {
+                player.setVelocityX(50);
+
+                player.anims.play('right', true);
+            }
+            else
+            {
+                player.setVelocityX(0);
+
+                player.anims.play('stand');
+            }
+        }
 }
 
 /*class InGame extends Phaser.Scene {
@@ -54,3 +74,24 @@ class LoadAssets extends Phaser.Scene {
 
     }
 }*/
+
+
+var config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 640,
+    pixelArt: true,
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { y: 0 },
+            debug: false
+        },
+    },
+    scene: [
+        LoadAssets/*,
+        InGame*/        
+    ],
+};
+
+var game = new Phaser.Game(config);
