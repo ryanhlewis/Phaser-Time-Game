91053c41530a2480e4ed7f4c1622aaabb2e421f8
AlyssaBurtscher <98368969+AlyssaBurtscher@users.noreply.github.com>
2022-05-02 15:14:44 -0500

    Background Fix

--- game.js
+++ game.js
@@ -116,7 +116,7 @@ class LoadAssets extends Phaser.Scene {
 		this.load.image('level2-middle', 'assets/maps/transparent.png');
 		this.load.image('level2-front', 'assets/maps/transparent.png');
         this.load.image('level2-background', 'assets/maps/Level2 Background.png')
-        this.load.image('level3-back', 'assets/maps/Level2 Background.png');
+        this.load.image('level3-back', 'assets/maps/transparent.png');
 		this.load.image('level3-middle', 'assets/maps/transparent.png');
 		this.load.image('level3-front', 'assets/maps/transparent.png');
         this.load.image('level3-background', 'assets/maps/transparent.png')
